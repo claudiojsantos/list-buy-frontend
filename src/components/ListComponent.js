f64edca767d3e3301105c0a40bde613034f6f6e3
Cláudio Santos <cljos@hotmail.com>
2020-10-24 17:32:37 -0300

    Criando o component List

---
+++ src/components/ListComponent.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function ListComponent(){
+    return (
+        <div>
+            <h2>Minha Lista</h2>
+            <ul>
+                <li>Item 1</li>
+                <li>Item 2</li>
+            </ul>
+        </div>
+    )
+}
+
+export default ListComponent
